edc24bb218c864abd0f15f0b0c3bc6eb4a26fc00
Nishka Sharma <nishkasharma21@gmail.com>
2026-02-05 02:03:55 -0800

    heh

--- app.py
+++ app.py
@@ -25,6 +25,4 @@ def home():
     return "Hello, Flask!"
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+     app.run(debug=True)
