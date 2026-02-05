56a82bc35735e75df33d5999351f5edc6fcb6480
Nishka Sharma <nishkasharma21@gmail.com>
2026-02-05 00:45:56 -0800

    Fix Heroku crash

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from backend.app import create_app
 
 app = create_app()
-print(app.url_map)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
