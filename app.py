350f1603e51831658334d6253b7bf91dc9f07eae
Nishka Sharma <nishkasharma21@gmail.com>
2026-02-05 01:59:51 -0800

    added saml.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Inky pinky ponky!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+from flask import Flask, session
+from saml_routes import saml_bp  # the file where your blueprint lives
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+
+# Register the blueprint
+app.register_blueprint(saml_bp, url_prefix='/saml')  # optional prefix
 
 @app.route("/")
 def home():
-    return "Inky pinky ponky!"
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
